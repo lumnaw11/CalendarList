3823edf88ec5af35baf510c1424d74d82e949676
Lum Naw <22cm0145@jec.ac.jp>
2024-02-15 12:39:24 +0900

    fix error

--- Sources/CalendarList/Data structures/CalendarEvent.swift
+++ Sources/CalendarList/Data structures/CalendarEvent.swift
@@ -10,48 +10,48 @@ import Foundation
 
 /// Type to store a speficic data point a given date. Date will be striped of hour/minute/second components.
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
-//public struct CalendarEvent<T:Hashable> {
-//    public var date:Date
-//    public var data:T
-//    
-//    public var calendar:Calendar
-//    
-//    public init(calendar:Calendar = Calendar.current, date:Date, data:T) {
-//        self.calendar = calendar
+public struct CalendarEvent<T:Hashable> {
+    public var date:Date
+    public var data:T
+
+    public var calendar:Calendar
+
+    public init(calendar:Calendar = Calendar.current, date:Date, data:T) {
+        self.calendar = calendar
+        self.data = data
+        self.date = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: date)!
+    }
+
+    public init(calendar:Calendar = Calendar.current, dateString:String, dateFormat:String = "MM/dd/yyyy", data:T) {
+        self.calendar = calendar
+        self.data = data
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        let date = dateFormatter.date(from:dateString)!
+
+        let components = calendar.dateComponents([.year, .month, .day], from: date)
+        self.date = calendar.date(from:components)!
+    }
+}
+//struct CalendarEvent: Codable, Identifiable {
+//    var id: String?
+//    var data: String
+//    var date: Date
+//
+//    init(data: String, date: Date = Date()) {
 //        self.data = data
-//        self.date = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: date)!
-//    }
-//    
-//    public init(calendar:Calendar = Calendar.current, dateString:String, dateFormat:String = "MM/dd/yyyy", data:T) {
-//        self.calendar = calendar
-//        self.data = data
-//        
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = dateFormat
-//        let date = dateFormatter.date(from:dateString)!
-//        
-//        let components = calendar.dateComponents([.year, .month, .day], from: date)
-//        self.date = calendar.date(from:components)!
+//        self.date = date
 //    }
 //}
-struct CalendarEvent: Codable, Identifiable {
-    var id: String?
-    var data: String
-    var date: Date
-    
-    init(data: String, date: Date = Date()) {
-        self.data = data
-        self.date = date
-    }
-}
-
-// Add the CalendarEvent extension for Hashable conformance
-extension CalendarEvent: Hashable {
-    static func == (lhs: CalendarEvent, rhs: CalendarEvent) -> Bool {
-        lhs.id == rhs.id
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
+//
+//// Add the CalendarEvent extension for Hashable conformance
+//extension CalendarEvent: Hashable {
+//    static func == (lhs: CalendarEvent, rhs: CalendarEvent) -> Bool {
+//        lhs.id == rhs.id
+//    }
+//
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(id)
+//    }
+//}
